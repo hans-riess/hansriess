d75aa63fbe4063f915b7651e37ea94a7719ad8b0
Hans Matthew Riess <39809993+hans-riess@users.noreply.github.com>
2025-06-23 15:05:44 -0400

    Revert "Dev"

--- academic/views.py
+++ academic/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Project, Profile
-
 def home(request):
-    projects = Project.objects.all()
-    profile = Profile.objects.first()  # Assuming only one profile
-    return render(request, 'index.html', {'projects': projects, 'profile': profile})
+    return render(request, 'index.html')
