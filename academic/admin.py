79e97c5c5503c317b5040fa0f06d30bf51ab71ad
hans-riess <hans.riess@duke.edu>
2025-06-23 13:44:13 -0400

    adding models back

--- academic/admin.py
+++ academic/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Project
+from .models import Project, Profile
 
-admin.site.register(Author)
 admin.site.register(Project)
+admin.site.register(Profile)
