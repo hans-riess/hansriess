fbef127b0e2a482f30b209573c07c4ecb57b3c06
hans-riess <hans.riess@duke.edu>
2025-06-23 14:04:08 -0400

    patch

--- academic/views.py
+++ academic/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Author,Project
 
 def home(request):
-    projects = Project.objects.all()
-    return render(request, 'index.html', {'projects': projects})
+    return render(request, 'index.html')
