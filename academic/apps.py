704f18b393e69d6651e2253a4676e38c98788e8a
hans-riess <hans.riess@duke.edu>
2025-08-05 12:18:37 -0400

    fixed apps.py

--- academic/apps.py
+++ academic/apps.py
@@ -8,10 +8,3 @@ class AcademicConfig(AppConfig):
     """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'academic'
-
-    def ready(self):
-        """
-        This method is called when Django starts.
-        We import our signals here to ensure they are registered.
-        """
-        import academic.signals
